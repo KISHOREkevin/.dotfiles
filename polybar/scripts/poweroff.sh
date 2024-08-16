02bc9cece8b236d199a1a54c8cefae6c7a787f47
KISHORE kevin <kishorekevin3289@gmail.com>
2024-08-16 15:37:55 +0530

    Update dotfiles

--- polybar/scripts/poweroff.sh
+++ polybar/scripts/poweroff.sh
@@ -1,17 +1,22 @@
 choice=0
+echo -e "Are you want to : \ny - poweroff\nr - reboot\nn - Cancel"
 while [ $choice = 0 ];
 do
-	read -p "Are you sure you want to shutdown [y/n] : " RESPONSE
-	if [ "$RESPONSE" = "y" ]
-	then
-        	shutdown now
-		choice=1
-	elif [ "$RESPONSE" = "n" ]
-	then
-        	echo "poweroff cancelled..."
-		choice=1
-	else 
-		echo "Enter a valid input !!!"
-		choice=0
-	fi
+    read -p "Enter the choice : " RESPONSE
+    if [ $RESPONSE = "y" ]
+    then
+            shutdown now
+            choice=1
+    elif [ $RESPONSE = "r" ]
+    then
+        shutdown -r now
+        choice=1
+    elif [ $RESPONSE = "n" ]
+    then
+	exit 0
+	choice=1
+    else 
+        echo "Enter a valid input !!!"
+        choice=0
+    fi
 done
