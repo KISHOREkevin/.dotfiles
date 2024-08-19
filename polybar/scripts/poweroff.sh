2e24bf18c905489bef16711a013723d8cf2f4c6f
KISHORE kevin <kishorekevin3289@gmail.com>
2024-08-19 18:03:25 +0530

    Update dotfiles

--- polybar/scripts/poweroff.sh
+++ polybar/scripts/poweroff.sh
@@ -3,28 +3,13 @@ echo -e "Are you want to : \n y - poweroff\n r - reboot\n l - logout\n s - sleep
 while [ $choice = 0 ];
 do
     read -p "Enter the choice : " RESPONSE
-    if [ $RESPONSE = "y" ]
-    then
-            shutdown now
-            choice=1
-    elif [ $RESPONSE = "r" ]
-    then
-        shutdown -r now
-        choice=1
-    elif [ $RESPONSE = "l" ]
-    then
-	i3 exit
-	choice=1
-    elif [ $RESPONSE = "s" ]
-    then
-	systemctl suspend
-	choice=1
-    elif [ $RESPONSE = "n" ]
-    then
-	exit 0
-	choice=1
-    else 
-        echo "Enter a valid input !!!"
-        choice=0
-    fi
+
+    case $RESPONSE in
+	y) shutdown now;choice=1;;
+	r) shutdown -r now;choice=1;;
+	l) i3 exit;choice=1;;
+	s) systemctl suspend;choice=1;;
+	n) exit 0;choice=1;;
+	*) echo "Enter a valid input !!!";choice=0;;
+    esac
 done
---
+++ polybar/scripts/sysinfo.sh
@@ -0,0 +1,11 @@
+fastfetch
+choice=0
+while [ $choice=0 ];do
+	echo -ne "Press q to exit \n"
+	read -n 1 response  
+	if [ $response = "q" ] 2> /dev/null ;
+	then
+		exit 0
+		choice=1
+	fi
+done
