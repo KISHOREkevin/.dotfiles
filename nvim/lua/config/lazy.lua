dbfc07e753892a2487adf43762d963f2490b386e
KISHORE kevin <kishorekevin3289@gmail.com>
2024-12-18 17:39:53 +0530

    Update dotfiles (tokyo-night)

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -39,7 +39,38 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 lsp.nvim_workspace()
 lsp.setup()
+local lspconfig = require("lspconfig")
+local configs = require("lspconfig/configs")
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+lspconfig.emmet_ls.setup({
+	-- on_attach = on_attach,
+	capabilities = capabilities,
+	filetypes = {
+		"css",
+		"eruby",
+		"html",
+		"javascript",
+		"javascriptreact",
+		"less",
+		"sass",
+		"typescript",
+		"scss",
+		"svelte",
+		"pug",
+		"typescriptreact",
+		"vue",
+	},
+	init_options = {
+		html = {
+			options = {
+				-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+				["bem.enabled"] = true,
+			},
+		},
+	},
+})
 -- keymaps for telescope--
 local builtin = require('telescope.builtin')
 
