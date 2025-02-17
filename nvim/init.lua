3dc9383fcf8f16f87af8a13609e74632a03b9efd
KISHORE kevin <kishorekevin3289@gmail.com>
2025-02-17 12:29:44 +0530

    update dotfiles

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,24 @@
 require("config.lazy")
 vim.o.cursorline=true
 vim.o.number=true
-
+vim.opt.clipboard="unnamedplus"
+vim.g.clipboard = {
+  name = "greenclip",
+  copy = {
+    ["+"] = "xclip -selection clipboard -in",
+    ["*"] = "xclip -selection clipboard -in",
+  },
+  paste = {
+    ["+"] = "greenclip print | head -n 1",
+    ["*"] = "greenclip print | head -n 1",
+  },
+  cache_enabled = 1,
+}
+vim.opt.fileencoding = "utf-8"
+vim.opt.hlsearch = true
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+vim.opt.smartcase = true 
+vim.opt.smartindent = true
+vim.opt.updatetime = 300
+vim.opt.scrolloff = 8
